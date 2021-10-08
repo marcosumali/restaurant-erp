38f7a90bd8a7bc9512870900bc382140651df2ae
Marco Sumali <marco.sumali@2359media.com>
2021-10-09 03:09:27 +0700

    chore: on progress login

--- server/routes/staff.js
+++ server/routes/staff.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const {
   register,
+  login,
 } = require('../controllers/staff')
 const {
   formCompleteness,
@@ -15,6 +16,7 @@ const router = express.Router()
 
 router
   .post('/register', formCompleteness, encryptPassword, catchError, register)
+  .post('/login', login)
 
 
 module.exports = router;
